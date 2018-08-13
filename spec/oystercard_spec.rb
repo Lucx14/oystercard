855998a05fcf1f844b7f7bec4eb67f1f81b16870
Lucien Najev <luciennajev@hotmail.com>
2018-08-13 19:49:48 +0100

    added @balance instance variable set to 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,15 @@ require 'oystercard'
 
 describe Oystercard do
 
+  describe "#Initialize" do
+
+    it "@balance - subject should respond to an instance variable @balance" do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it "@balance - has a balance of zero" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
 end
